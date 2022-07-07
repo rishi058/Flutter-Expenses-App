3741c24d7bd8e457effca5df638674d7c9b33312
Rishi Raj <97884033+rishi058@users.noreply.github.com>
2022-07-07 12:43:32 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+
+import './widgets/new_transaction.dart';
+import './widgets/transaction_list.dart';
+import './widgets/chart.dart';
+import 'my_drawer.dart';
+import 'package:flutter_complete_guide/sql_helper.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Personal Expenses',
+      theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blueGrey)
+              .copyWith(secondary: Colors.amber)),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  // String titleInput;
+  // String amountInput;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  List<Map<String,dynamic>> _userTransactions = [];
+
+  void _refreshNotes() async {
+
+    final data = await SQLHelper.getItems();
+
+    setState(() {
+      _userTransactions = data ;
+    });
+
+  }
+
+  @override
+
+  void initState(){
+    super.initState();
+    _refreshNotes();
+    // print(_userTransactions);
+  }
+
+
+  List<Map<String,dynamic>> get _weeklyTransactions {
+    return _userTransactions.where((tx) {
+      return DateTime.parse(tx['raw_date']).isAfter(
+        DateTime.now().subtract(
+          Duration(days: 7),
+        ),
+      );
+    }).toList();
+  }
+
+  Future<void> _addNewTransaction(
+      String Title, int Amount, String raw_date, String view_date
+      ) async {
+    await SQLHelper.createItem(Title, Amount, raw_date,  view_date);
+    _refreshNotes();
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+      context: ctx,
+      builder: (_) {
+        return GestureDetector(
+          onTap: () {},
+          child: NewTransaction(_addNewTransaction),
+          behavior: HitTestBehavior.opaque,
+        );
+      },
+    );
+  }
+
+  void _deleteTransaction(int id) async {
+    await SQLHelper.deleteItem(id);
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+      content: Text('Successfully deleted'),
+    ));
+    _refreshNotes();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text(
+          'Personal Expenses',
+          style: TextStyle(
+            fontSize: 23,
+            color: Colors.black.withOpacity(0.6),
+          ),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () => _startAddNewTransaction(context),
+          ),
+        ],
+      ),
+
+      drawer : MainDrawer(),
+
+      body: RawScrollbar(
+        thumbVisibility: true,
+        thumbColor: Colors.blueGrey.shade200,
+        thickness: 8,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+
+              Chart(_weeklyTransactions),
+              TransactionList(_userTransactions, _deleteTransaction),
+            ],
+          ),
+        ),
+      ),
+
+    );
+  }
+}
---
+++ my_drawer.dart
@@ -0,0 +1,273 @@
+import 'package:flutter/material.dart';
+import 'main.dart';
+import 'about.dart';
+import 'Monthly.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter_complete_guide/sql_helper.dart';
+
+class MainDrawer extends StatefulWidget {
+
+  @override
+  State<MainDrawer> createState() => _MainDrawerState();
+}
+
+class _MainDrawerState extends State<MainDrawer> {
+
+  List<Map<String, dynamic>> trans = [];
+
+  void refresh() async {
+    final data = await SQLHelper.getItems();
+    trans = data ;
+  }
+
+  void _deleteTransaction(int id) async {
+    await SQLHelper.deleteItem(id);
+  }
+
+  void deleteAll() {
+    for(int i=0; i<trans.length; i++){
+      int idd = trans[i]['id'];
+      _deleteTransaction(idd);
+    }
+
+    Navigator.of(context, rootNavigator: true).pop(context);
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => MyApp()),
+    );
+
+  }
+
+  void cache(){
+    var date = int.parse(DateFormat.d().format(DateTime.now()).toString());
+    var late_month = DateTime.now().subtract(Duration(days: date+61));
+    for (int i = 0; i < trans.length; i++) {
+      if(DateTime.parse(trans[i]['raw_date']).month<=late_month.month){
+          int idd = trans[i]['id'];
+          _deleteTransaction(idd);
+      }
+
+    }
+
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => MyApp()),
+    );
+
+  }
+
+  void showCustomDialog(BuildContext context) {
+    showGeneralDialog(
+      context: context,
+      barrierLabel: "Barrier",
+      barrierDismissible: true,
+      barrierColor: Colors.black.withOpacity(0.5),
+      transitionDuration: Duration(milliseconds: 250),
+      pageBuilder: (_, __, ___) {
+        return Center(
+          child: Container(
+            height: 180,
+            margin: EdgeInsets.all(15),
+            decoration: BoxDecoration(color: Colors.white, borderRadius: BorderRadius.circular(25)),
+            child: Container(
+              margin: EdgeInsets.all(15),
+              child: Scaffold(
+                body: Container(
+                  //margin: EdgeInsets.all(10),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        child: Text('Delete Everything',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25,
+                        ),
+                        ),
+                      ),
+                      Container(
+                        child: Text('Are you sure? \n\nTap on Yes to Delete everything you have saved till now.\n'),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          SizedBox(
+                            height: 50,
+                            width: 90,
+                            child: ElevatedButton(
+                                onPressed: (){
+                                  Navigator.of(context, rootNavigator: true).pop(context);
+                                },
+                                child: Text('No',
+                                style: TextStyle(
+                                  fontSize: 24
+                                ),
+                                ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 50,
+                            width: 90,
+                            child: ElevatedButton(
+                              onPressed: (){
+                                deleteAll();
+
+                              },
+                              child: Text('Yes',
+                                style: TextStyle(
+                                    fontSize: 24
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+      transitionBuilder: (_, anim, __, child) {
+        Tween<Offset> tween;
+        if (anim.status == AnimationStatus.reverse) {
+          tween = Tween(begin: Offset(-1, 0), end: Offset.zero);
+        } else {
+          tween = Tween(begin: Offset(1, 0), end: Offset.zero);
+        }
+
+        return SlideTransition(
+          position: tween.animate(anim),
+          child: FadeTransition(
+            opacity: anim,
+            child: child,
+          ),
+        );
+      },
+    );
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    refresh();
+    return Drawer(
+        child: Container(
+          color: Colors.blueGrey.shade50,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+
+
+                Container(
+                  height: 200,
+                  width: double.infinity,
+                  child: Stack(
+                    children: [
+
+                      Container(
+                          child: Image.asset(
+                            'assets/images/1234.jpg',
+                            fit: BoxFit.cover,
+                          ),
+                      ),
+                      Container(
+                        child: Positioned(
+                          bottom : 10,
+                          left : 0,
+                          child: Container(
+                            color: Colors.purpleAccent.shade100.withOpacity(0.5),
+                            child: Text('Personal Expenses',
+                              style: TextStyle(fontSize: 25, fontWeight: FontWeight.w200),
+                              textAlign: TextAlign.end,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                InkWell(
+                  onTap: (){
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => monthly()),
+                    );
+                  },
+                  child: Container(
+                    height: 60,
+                    margin: EdgeInsets.all(5),
+                    child: Card(
+                      elevation: 0,
+                      child: ListTile(
+                        leading: Icon(Icons.wallet),
+                        title: Text('Monthly Expenses',),
+                      ),
+                    ),
+                  ),
+                ),
+
+                InkWell(
+                  onTap: (){
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => About()),
+                    );
+                  },
+                  child: Container(
+                    height: 60,
+                    margin: EdgeInsets.all(5),
+                    child: Card(
+                      elevation: 0,
+                      child: ListTile(
+                        leading: Icon(Icons.info),
+                        title: Text('About',),
+                      ),
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: (){
+                    cache();
+                  },
+                  child: Container(
+                    height: 60,
+                    margin: EdgeInsets.all(5),
+                    child: Card(
+                      elevation: 0,
+                      child: ListTile(
+                        leading: Icon(Icons.delete_sweep_outlined),
+                        title: Text('Clear Cache',),
+                      ),
+                    ),
+                  ),
+                ),
+
+                InkWell(
+                  onTap: (){
+                    showCustomDialog(context);
+                  },
+                  child: Container(
+                    height: 60,
+                    margin: EdgeInsets.all(5),
+                    child: Card(
+                      elevation: 0,
+                      child: ListTile(
+                        leading: Icon(Icons.delete),
+                        title: Text('Delete All Data',),
+                      ),
+                    ),
+                  ),
+                ),
+
+              ],
+            ),
+          ),
+        ),
+    );
+  }
+}
